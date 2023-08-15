5f7a3a9260ef81d6a9f350acde89fe72c6f8d12c
Johan Lyheden <github-johan@lyheden.com>
2023-08-15 09:11:27 +0200

    Update CI

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,16 +7,6 @@ usage() {
     exit 1
 }
 
-if [ -z "$B2_APPLICATION_KEY" ]; then
-    echo "Missing B2_APPLICATION_KEY env var"
-    exit 1
-fi
-
-if [ -z "$B2_KEY_ID" ]; then
-    echo "Missing B2_KEY_ID env var"
-    exit 1
-fi
-
 if [ -z "$B2_BUCKET_NAME" ]; then
     echo "Missing B2_BUCKET_NAME env var"
     exit 1
@@ -32,7 +22,7 @@ echo $REF > version.txt
 
 BUCKET_URL="b2://${B2_BUCKET_NAME}/"
 
-b2 authorize-account $B2_KEY_ID $B2_APPLICATION_KEY 2>&1 >/dev/null
+b2 authorize-account 2>&1 >/dev/null
 b2 sync \
     --delete \
     --replaceNewer \
